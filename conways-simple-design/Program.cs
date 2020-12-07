7dcc9454982c9b9bf687d335a28252ab76683373
Lanzhou Jiang <lzjiang99@gmail.com>
2020-12-07 11:08:40 +1100

    initial commit

---
+++ conways-simple-design/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace conways_simple_design
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
